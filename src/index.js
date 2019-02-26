c4315263a3e2dbcb7c5e1af72ebb09e05ea87913
fagner-daitan <faraujo@daitan.com>
2019-02-26 15:23:50 -0300

    Improve list style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import HandleListCommands from './js/core/handle-form-commands.js';
-import refreshPeopleList from './js/core/refresh-people-list.js';
 
 window.countPeople = 1;
 window.actualPage = 0;
 window.perPage = 3;
 
 HandleListCommands();
-refreshPeopleList();
+// refreshPeopleList();
