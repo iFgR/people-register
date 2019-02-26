3d50adb294cc283fa4732adda714b7071908b9a2
fagner-daitan <faraujo@daitan.com>
2019-02-26 16:21:22 -0300

    Final touches on behaviors & styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import HandleListCommands from './js/core/handle-form-commands.js';
+import RefreshPeopleList from './js/core/refresh-people-list.js';
 
 window.countPeople = 1;
 window.actualPage = 0;
 window.perPage = 3;
 
 HandleListCommands();
-// refreshPeopleList();
+RefreshPeopleList();
--- src/js/core/add-people-card.js
+++ src/js/core/add-people-card.js
@@ -2,24 +2,36 @@ export default personInfo => {
   const listContainer = document.querySelector('.list');
   const listItem = document.createElement('li');
   listItem.className = 'card-list';
-  listItem.innerHTML = tplPeopleCard(personInfo);
+  const formattedPersonInfo = formatTemplateInfo(personInfo);
+  console.log(formattedPersonInfo);
+  listItem.innerHTML = tplPeopleCard(formattedPersonInfo);
   listContainer.appendChild(listItem);
 };
 
-const tplPeopleCard = ({ id, personName, phone, isActive, cpf, birthday }) => `
-  <div class="status">
-    ${isActive}
+const formatTemplateInfo = (personInfo) => ({
+  ...personInfo,
+  activeIcon: personInfo.isActive ? '<i class="fas fa-check-square fa-2x"></i>' : '<i class="far fa-check-square fa-2x"></i>'
+})
+
+const tplPeopleCard = ({ id, personName, phone, activeIcon, isActive, cpf, birthday, gender, email }) => `
+  <div class="status ${isActive ? '-active' : ''}">
+    ${activeIcon}
   </div>
   <div class="identification">
-    <h4>${personName}</h4>
-    <h6>${cpf}</h6>
+    <h4 title="Nome" class="personname"><i class="fas fa-user-alt fa-sm icon"></i>${personName}</h4>
+    <h5 title="e-mail" class="email"><i class="fas fa-envelope fa-sm icon"></i>${email}</h6>
+    <div class="more-info">
+      <h5 title="CPF"><i class="fas fa-id-card fa-sm icon"></i>${cpf}</h6>
+      <h5 title="Telefone"><i class="fas fa-phone icon"></i>${phone}</h6>
+    </div>
   </div>
-  <div class="info">
-    <h6>${birthday}</h6>
-    <h6>${phone}</h6>
+  <div class="information">
+    <h4 class="birthday"><i class="far fa-calendar-alt fa-sm icon"></i>${birthday}</h5>
+    <h6>${gender === 'male' ? 'Masculino' : 'Feminino'}</h5>
   </div>
+
   <div class="commands">
-    <button type="button" onclick="editPerson('${id}')">Editar</button>
-    <button type="button" onclick="removePerson('${id}')">Remover</button>
+    <button class="btn -primary command" onclick="editPerson('${id}')">Editar</button>
+    <button class="btn -default command" onclick="removePerson('${id}')">Remover</button>
   </div>
 `;
